f4f4eeae4fc3b141e770d1d42afb96bb7869de64
Daniel Sávio <spnjc137@gmail.com>
2024-10-20 13:13:26 -0300

    Escondendo os filtros da tela home, pois eles ainda não existem

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -34,9 +34,9 @@ const home: React.FC = () => {
     <div className=" text-zinc-600 h-screen bg-[url('https://img.freepik.com/vetores-gratis/estilo-de-fundo-branco-textura-elegante_23-2148432200.jpg?w=1380&t=st=1729217077~exp=1729217677~hmac=ede31c1c617b859ed0a00cafed97fdd527c1d1fcf7908cf75a41b29d2206ef2f')] bg-cover pt-20 px-2 flex flex-col items-center">
       <div className="absolute bottom-10 md:right-24 z-10  ">
         <Popover>
-          <PopoverTrigger className="p-2 rounded-full bg-blue-700 flex justify-center md:w-[90px] items-center z-1">
+          {/* <PopoverTrigger className="p-2 rounded-full bg-blue-700 flex justify-center md:w-[90px] items-center z-1">
             <FilterIcon className="text-zinc-50 md:size-8"></FilterIcon>
-          </PopoverTrigger>
+          </PopoverTrigger> */}
 
           <PopoverContent className="flex flex-col items-center bg-gradient-to-tl from-blue-200 to-zinc-50 md:mr-4">
             <h1 className="font-bold text-lg">Localidade</h1>
