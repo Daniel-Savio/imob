cf735b1c396297e84ed5a004652d91c047e38a71
Daniel Savio <daniel.savio@treetech.com.br>
2024-10-21 17:51:25 -0300

    Adicionando um spinner enquanto os dados não carregam

--- src/App.tsx
+++ src/App.tsx
@@ -10,45 +10,50 @@ import About from "./pages/locked";
 import { Tabs, TabsList, TabsTrigger } from "./components/ui/tabs";
 import Admin from "./pages/admin";
 import { Buildings } from "phosphor-react";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <BrowserRouter>
-      <Toaster />
-      <header className=" fixed top-0 flex justify-center w-full align-middle text-zinc-600">
-        <Tabs
-          className="relative block justify-center text-center w-full"
-          defaultValue="home"
-        >
-          <div className="flex gap-2 justify-center bg-zinc-100 py-1 m-auto w-full align-bottom">
-            <Buildings className="w-8 h-8 mt-auto ml-2" weight="regular" />
-            <h1 className="flex text-nowrap mt-auto text-lg">
-              Daniel Pinheiro Corretagem
-            </h1>
-          </div>
-          <TabsList className=" flex bg-transparent w-full gap-4 justify-center">
-            <Link to="/">
-              <TabsTrigger className="rounded-full  px-5" value="home">
-                <HomeIcon className="h-5 w-fit" />
-              </TabsTrigger>
-            </Link>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <Toaster />
+        <header className=" fixed top-0 flex justify-center w-full align-middle text-zinc-600">
+          <Tabs
+            className="relative block justify-center text-center w-full"
+            defaultValue="home"
+          >
+            <div className="flex gap-2 justify-center bg-zinc-100 py-1 m-auto w-full align-bottom">
+              <Buildings className="w-8 h-8 mt-auto ml-2" weight="regular" />
+              <h1 className="flex text-nowrap mt-auto text-lg">
+                Daniel Pinheiro Corretagem
+              </h1>
+            </div>
+            <TabsList className=" flex bg-transparent w-full gap-4 justify-center">
+              <Link to="/">
+                <TabsTrigger className="rounded-full  px-5" value="home">
+                  <HomeIcon className="h-5 w-fit" />
+                </TabsTrigger>
+              </Link>
 
-            <Link to="/locked">
-              <TabsTrigger className="rounded-full  px-5" value="vendor">
-                <LockKeyhole className="h-5 w-fit" />
-              </TabsTrigger>
-            </Link>
-          </TabsList>
-        </Tabs>
-      </header>
-      <section>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/locked" element={<About />} />
-          <Route path="/admin" element={<Admin />} />
-        </Routes>
-      </section>
-    </BrowserRouter>
+              <Link to="/locked">
+                <TabsTrigger className="rounded-full  px-5" value="vendor">
+                  <LockKeyhole className="h-5 w-fit" />
+                </TabsTrigger>
+              </Link>
+            </TabsList>
+          </Tabs>
+        </header>
+        <section>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/locked" element={<About />} />
+            <Route path="/admin" element={<Admin />} />
+          </Routes>
+        </section>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
 
--- src/components/adminImovelList.tsx
+++ src/components/adminImovelList.tsx
@@ -1,34 +1,43 @@
 import { Imovel } from "@/schemas/imovelScheema";
 import axios from "axios";
-
-import { useEffect, useState } from "react";
+import { useQuery } from "@tanstack/react-query";
 import ImovelCard from "./ui/imovelCard";
 import { apiUrl } from "@/utils";
 import { ScrollArea } from "@radix-ui/react-scroll-area";
+import { Spinner } from "phosphor-react";
+
+// Função para buscar dados
+const fetchImoveis = async () => {
+  const { data } = await axios.get<Imovel[]>(apiUrl);
+  return data;
+};
 
 export default function AdminImovelList() {
-  const [list, setList] = useState<Imovel[] | undefined>();
+  // Usando o React Query para lidar com a requisição
+  const {
+    data: list,
+    isLoading,
+    error,
+  } = useQuery({
+    queryKey: ["imoveis"],
+    queryFn: fetchImoveis,
+  });
 
-  useEffect(() => {
-    async function getList() {
-      const listData: Imovel[] = (await axios.get(apiUrl)).data;
+  if (isLoading) {
+    return <Spinner className="animate-spin h-10 w-10 mt-24"></Spinner>;
+  }
 
-      setList(listData);
-    }
-    setTimeout(() => {
-      getList();
-    });
-  }, []);
+  if (error) {
+    return <div>Erro ao carregar imóveis</div>;
+  }
 
   return (
     <ScrollArea className="flex flex-wrap justify-center max-h-screen overflow-y-scroll w-full pb-72">
-      {list?.map((imovel) => {
-        return (
-          <div className="m-2">
-            <ImovelCard key={imovel.id} imovelId={imovel.id} />
-          </div>
-        );
-      })}
+      {list?.map((imovel) => (
+        <div key={imovel.id} className="m-2">
+          <ImovelCard imovelId={imovel.id} />
+        </div>
+      ))}
     </ScrollArea>
   );
 }
