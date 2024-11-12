a619c0a25fdd201bfece43c19d4fdb9979311c61
Daniel Savio <daniel.savio@treetech.com.br>
2024-11-12 08:26:46 -0300

    Locked page: adjusting form center

--- src/pages/locked.tsx
+++ src/pages/locked.tsx
@@ -2,8 +2,8 @@ import LoginForm from "@/components/forms/loginForm";
 
 export default function About() {
   return (
-    <div className="h-screen flex flex-col items-center">
-      <section className="m-auto">
+    <div className="h-[80vh] flex flex-col items-center">
+      <section className="my-auto">
         <LoginForm></LoginForm>
       </section>
     </div>
